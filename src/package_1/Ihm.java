30ed59b9474f7fefbbe040c0dc76e49ad17453f7
Noémie <nonoemielv.15@gmail.com>
2021-12-23 19:33:35 +0100

    ihm numero 1

--- src/package_1/Ihm.java
+++ src/package_1/Ihm.java
@@ -1,77 +1,85 @@
 package package_1;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Ihm {
-    public int choisirGroupe1(){
-        Scanner saisieUtilisateur = new Scanner(System.in);
-        System.out.println("Etes vous un étudiant ou un personnel ?");
-        System.out.println("Ecriver 1 pour étudiant et 2 pour personnel");
-        int EouP = saisieUtilisateur.nextInt();
-        return EouP;
-    }
-    public boolean ChoisirGroupe(){
-        Scanner saisieUtilisateur = new Scanner(System.in);
-        int EouP=choisirGroupe1();
-        System.out.println("Veiller écrire votre numero d'identification");
-        int NumeroDonée = saisieUtilisateur.nextInt();
-        if(EouP == 1){
-            if(chercherEtudiant(NumeroDonée)){
-                return true;
-            }
-            else {
-                return false;
-            }
-        }else if (EouP == 2){
-            if (chercherPersonnel(NumeroDonée)){
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        return false;//j'ai mis ca car je ne savais pas quoi mettre comme exception ici
-    }
-    public String PasInscris (){
-        Scanner saisieUtilisateur = new Scanner(System.in);
-        System.out.println("voulez-vous vous inscrire ou voulez vous quitter l'application ?");
-        System.out.println("Ecriver 1 pour vous inscrire et 2 pour quitter l'application");
-        int IouA = saisieUtilisateur.nextInt();
-        int EouP=choisirGroupe1();
-        if(IouA==1){
-            if(EouP==1){//inscripition d'un Etudiant qui n'est pas dans la liste
-                System.out.println("veuillez saisir votre numero");
-                int NumeroEtu = saisieUtilisateur.nextInt();
-                System.out.println("veuiller saisir votre nom");
-                String NomEtu = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre prenom");
-                String PrenomEtu = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre email");
-                String EmailEtu = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre tel");
-                int telEtu = saisieUtilisateur.nextInt();
-                System.out.println("veuillez saisir votre année");
-                int AnnéeEtu= saisieUtilisateur.nextInt();
-                inscriptionEtudiant(NumeroEtu,NomEtu,PrenomEtu,EmailEtu,telEtu,AnnéeEtu);//faut juste qu'elle soit implementé
-            }
-            else if(EouP==2){//inscription d'un personnel qui n'est pas dans la liste
-                System.out.println("veuillez saisir votre numero");
-                int NumeroPer = saisieUtilisateur.nextInt();
-                System.out.println("veuiller saisir votre nom");
-                String NomPer = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre prenom");
-                String PrenomPer = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre email");
-                String EmailPer = saisieUtilisateur.next();
-                System.out.println("veuillez saisir votre tel");
-                int telPer = saisieUtilisateur.nextInt();
-                inscriptionPersonnel(NumeroPer,NomPer,PrenomPer,EmailPer,telPer);//faut juste qu'elle soit implementé
-            }
+    public Scanner scan = new Scanner(System.in);
 
-        }else if(IouA==2){
-            System.exit(0);
+    public int etudiantOuPersonnel() {
+        while (true) {
+            System.out.println("Etes-vous étudiant ou personnel ? Entrez 1 pour étudiant, 2 pour personnel.");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Entrez un chiffre /!\\");
+            }
         }
     }
 
+    public int identification() {
+        while (true) {
+            System.out.println("Veuillez vous identifier avec votre numéro.");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Entrez votre numéro /!\\");
+            }
+        }
+    }
 
+    public int inscriptionOuQuitter() {
+        while (true) {
+            System.out.println("Voulez-vous vous inscrire ou quitter ? Entrez 1 pour inscription, 2 pour quitter");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Veuillez rentrer 1 pour incription ou 2 pour quitter /!\\");
+            }
+        }
+    }
+
+    public int menuGestion() {
+        while (true) {
+            System.out.println("1 – Gérer les places du dîner 2 – Se désinscrire 3 – Quitter");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Veuillez rentrer soit 1 pour gérer les places du dîner, 2 se désinscrire, 3 pour quitter /!\\");
+            }
+        }
+    }
+
+    public int consulterPlanTable() {
+        while (true) {
+            System.out.println("Voulez-vous consulter le plan de table ? Entrez 1 pour oui ou 2 pour non");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Veuillez rentrer 1 pour oui ou 2 pour non /!\\");
+            }
+        }
+    }
+
+    public int choixTable() {
+        while (true) {
+            System.out.println("Choisissez un numéro de table.");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Veuillez rentrer un numéro de table /!\\");
+            }
+        }
+    }
+
+    public int choixPlaces() {
+        while (true) {
+            System.out.println("Choisissez un nombre de place.");
+            if (scan.hasNextInt()) {
+                return scan.nextInt();
+            } else {
+                System.out.println("/!\\ Veuillez rentrer un nombre de place /!\\");
+            }
+        }
+    }
 }
